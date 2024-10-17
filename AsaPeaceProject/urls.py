3bc82d13578c92d05eda304da40e42a45ab06afb
Cardonaran <102301018+anfecaes@users.noreply.github.com>
2024-10-16 21:04:54 -0300

    Delete AsaPeaceProject directory

--- AsaPeaceProject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from main import views  # Importa tus vistas de la aplicación "main"
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.homepage, name='homepage'),  # Ruta para la página principal
-]
